76248e88878695a26cbd5881fe06661c31c3d98c
deltron-fr <kayceeogbonnaya2304@gmail.com>
2024-09-14 21:02:44 +0100

    Updated the User Profile

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
 
 class RegisterForm(UserCreationForm):
     """Form to Create new User"""
@@ -10,3 +11,16 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+class UserUpdateForm(forms.ModelForm):
+    usable_password = None
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ["username", "email"]
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
